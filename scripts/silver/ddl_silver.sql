2e89b9a4ef27921e7af2fde9623df7d5ac185200
yash7raut@gmail.com <yash7raut@gmail.com>
2026-09-21 17:40:03 +0000

    added ddl statements for the silver layer

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,95 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'silver' Tables
+===============================================================================
+*/
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.crm_cust_info (
+    cst_id              INT,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE,
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)
+TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)
+TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.erp_loc_a101 (
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50),
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)
+TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.erp_cust_az12 (
+    cid    VARCHAR(50),
+    bdate  DATE,
+    gen    VARCHAR(50),
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)
+TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
+
+CREATE TABLE IF NOT EXISTS datawarehouse.silver.erp_px_cat_g1v2 (
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50),
+    dwh_create_date     DATE DEFAULT CURRENT_DATE()
+)
+TBLPROPERTIES (
+    'delta.minReaderVersion' = '2',
+    'delta.minWriterVersion' = '7',
+    'delta.feature.allowColumnDefaults' = 'enabled'
+);
